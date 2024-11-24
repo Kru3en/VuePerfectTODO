ac3a8603b1c83b282587b73a7fff6edcba6bc1fb
Игорь Городничук <gorodni4uk.igor@mail.ru>
2024-11-25 01:53:59 +0300

    Feat: end task 6(sum test rabotaet)

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,5 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+const sum = require("./sum");
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
-  });
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
 });
